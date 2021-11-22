1e28878069fc32118c7e4cf75386926d5a44b69b
Patrick Reith <preith421@gmail.com>
2021-11-22 11:45:01 -0500

    fixed typo

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,8 @@ var heading = 1;
 
 
 // 
-document.getElementById(mybutton).onClick(alert("WOW!"));
+//document.getElementById(mybutton).onClick(alert("WOW!"));
+
+function myFunction(){
+	alert("WOW!");
+}
